f1642f8f82732d578ea3b75002389a941ead98e1
luidimso <luidi_mso@outlook.com>
2020-08-22 23:39:54 -0300

    Navbar completed

--- src/app/components/base/navbar/navbar.component.ts
+++ src/app/components/base/navbar/navbar.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  expanded:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  expand() {
+    this.expanded == true ? this.expanded = false : this.expanded = true;
   }
 
 }
